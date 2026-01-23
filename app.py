aa3f9eb72c712b73c2d73f4fbff7010f7e3d20be
Jakob Lange <jakob.lange@kartverket.no>
2026-01-23 13:54:59 +0100

    update app for fun

--- app.py
+++ app.py
@@ -1,19 +1,29 @@
 import os
 
-from flask import Flask, url_for
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 
+def get_secret():
+    return "\n".join(str(os.environ.get(f"line_{i}")) for i in range(1, 5))
+
+
 @app.route("/")
 def index():
-    secret_link = url_for("secret")
-    return f"<h1>Hello!</h1><p><a href='{secret_link}'>Watch the secret here!</a>"
+    return render_template("index.html")
 
 
-@app.route("/secret")
-def secret():
-    return str(os.environ.get("my_secret"))
+@app.route("/yes")
+def reveal_secret():
+    secret_value = get_secret()
+    return render_template("secret.html", secret=secret_value)
 
 
-app.run()
+@app.route("/no")
+def hide_secret():
+    return render_template("index.html")
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
